23034da80d45a6d0bf72765cdaf06fcfe38cb3f3
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-01-24 22:55:06 +0100

    Handle new lines

--- terminal.c
+++ terminal.c
@@ -73,15 +73,27 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
+
+void terminal_newline(void){
+  terminal_column = 0;
+  if (++terminal_row == VGA_HEIGHT)
+    terminal_row = 0;
+}
+
 void terminal_putchar(char c) 
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+  switch(c){
+   case '\n':
+     terminal_newline();
+     break;
+     
+  default:
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+      terminal_newline();
+    }
+    break;
+  }
 }
  
 void terminal_write(const char* data, size_t size) 
