2b5ca7d15260190b748e9c0f0c541d55b6197cf4
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-02-04 15:59:50 +0100

    Add interrupt.s

---
+++ interrupt.s
@@ -0,0 +1,14 @@
+.global interrupt_handler
+
+interrupt_handler:
+         pusha
+        cld
+        //jmp interrupt_handler2
+        popa
+        iret
+        nop
+interrupt_handler2:
+        jmp interrupt_handler3
+        nop
+interrupt_handler3:
+        hlt
