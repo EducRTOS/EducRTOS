3303164d35bf47382a6f47d01472ab035ea9c723
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-02-18 14:27:31 +0100

    Simple context switching

--- application.c
+++ application.c
@@ -3,13 +3,26 @@
 #include "high_level.h"
 
 #define USER_STACK_SIZE 1024
-static char user_stack[USER_STACK_SIZE] __attribute__((aligned(16)));
+static char user_stack0[USER_STACK_SIZE] __attribute__((aligned(16)));
+static char user_stack1[USER_STACK_SIZE] __attribute__((aligned(16)));
 
-struct context ctx0;
+struct context ctx0, ctx1;
 
-void test_userspace(void){
+void test_userspace0(void){
   for(int i = 0; i < 3; i++){
-    terminal_writestring("Hello from userspace ");
+    terminal_writestring("Hello from userspace0 ");
+    terminal_write_uint32(i);
+    terminal_writestring("\n");
+    yield();
+    terminal_writestring("Ret\n");
+  }
+  while(1);
+}
+
+
+void test_userspace1(void){
+  for(int i = 0x10; i < 0x13; i++){
+    terminal_writestring("Hello from userspace1 ");
     terminal_write_uint32(i);
     terminal_writestring("\n");
     yield();
--- high_level.c
+++ high_level.c
@@ -1,6 +1,10 @@
 #include "high_level.h"
 #include "terminal.h"
 
+/* For now: the application is not separated. */
+#include "application.c"
+
+
 void __attribute__((fastcall,noreturn))
 high_level_syscall(struct hw_context *cur_hw_ctx, int syscall_number){
   /* This works because hw_context must be the first field of a context. */
@@ -9,13 +13,14 @@ high_level_syscall(struct hw_context *cur_hw_ctx, int syscall_number){
 
   terminal_writestring("Calling interrupt\n");
   terminal_write_uint32(syscall_number);
-  hw_context_switch(&cur_ctx->hw_context);
 
-  
+  if(cur_ctx == &ctx0)
+    hw_context_switch(&ctx1.hw_context);
+  else
+    hw_context_switch(&ctx0.hw_context);
+    /* hw_context_switch(&cur_ctx->hw_context); */    
 }
 
-/* For now: the application is not separated. */
-#include "application.c"
 
 void context_init(struct context *ctx, uint32_t sp, uint32_t pc){
   hw_context_init(&ctx->hw_context, sp, pc);
@@ -24,7 +29,10 @@ void context_init(struct context *ctx, uint32_t sp, uint32_t pc){
 void __attribute__((noreturn))
 high_level_init(void){
   context_init(&ctx0,
-               (uint32_t) &user_stack[USER_STACK_SIZE - sizeof(uint32_t)],
-               (uint32_t) &test_userspace);
+               (uint32_t) &user_stack0[USER_STACK_SIZE - sizeof(uint32_t)],
+               (uint32_t) &test_userspace0);
+  context_init(&ctx1,
+               (uint32_t) &user_stack1[USER_STACK_SIZE - sizeof(uint32_t)],
+               (uint32_t) &test_userspace1);
   hw_context_switch(&(ctx0.hw_context));
 }
