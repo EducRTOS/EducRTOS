9dafbb210c1296bceb11b7c3ce42a68ec5741e46
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-02-25 13:22:22 +0100

    Export

--- terminal.h
+++ terminal.h
@@ -7,4 +7,5 @@ void terminal_initialize(void);
 void terminal_print(char * format,...) __attribute__ ((format (printf, 1, 2)));
 void terminal_writestring(const char* data);
 void terminal_write_uint32(uint32_t num);
+void terminal_putchar(unsigned char c);
 #endif
