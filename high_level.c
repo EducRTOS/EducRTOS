31bfcf11856fe4bd64bdec5f07be29e5d546e09a
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-03-06 02:33:10 +0100

    Initialize the scheduler

--- high_level.c
+++ high_level.c
@@ -37,26 +37,21 @@ high_level_timer_interrupt_handler(struct hw_context *cur_hw_ctx){
 
 void context_init(struct context * const ctx, int idx,
                   uint32_t pc,
-                  uint32_t start, uint32_t end,
-                  struct context * const prev){
+                  uint32_t start, uint32_t end) {
   hw_context_init(&ctx->hw_context, idx, pc, start, end);
-#ifdef ROUND_ROBIN_SCHEDULING  
-  prev->sched_context.next = ctx;
-#endif  
 }
 
 void __attribute__((noreturn))
 high_level_init(void){
   unsigned int const nb_tasks = user_tasks_image.nb_tasks;
-
-  /* Initialize the circular list of contexts. */
-  struct context * prev = user_tasks_image.tasks[nb_tasks - 1].context;
+  
   for(unsigned int i = 0; i < nb_tasks; i++){
     struct task_description const *task = &user_tasks_image.tasks[i];
     context_init(task->context, i, task->start_pc,
-                 (uint32_t) task->task_begin, (uint32_t) task->task_end,
-                 prev);
-    prev = task->context;
+                 (uint32_t) task->task_begin, (uint32_t) task->task_end);
   }
+
+  scheduler_init();
+  
   hw_context_switch(&user_tasks_image.tasks[0].context->hw_context);
 }
--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,24 @@
 #include "scheduler.h"
 #include "high_level.h"
+#include "user_tasks.h"
 
 #ifdef ROUND_ROBIN_SCHEDULING
 struct context *sched_choose_from(struct context *ctx){
   return ctx->sched_context.next;
 }
+
+void scheduler_init(void){
+  unsigned int const nb_tasks = user_tasks_image.nb_tasks;
+
+  /* Initialize the circular list of contexts. */  
+  struct context * prev = user_tasks_image.tasks[nb_tasks - 1].context;
+  for(unsigned int i = 0; i < nb_tasks; i++){
+    struct task_description const *task = &user_tasks_image.tasks[i];    
+    struct context *ctx = task->context;
+    prev->sched_context.next = ctx;
+    prev = task->context;
+  }
+}
+
+
 #endif
