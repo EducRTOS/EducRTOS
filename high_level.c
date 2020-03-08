ca6823f5e6d65057efb2a44b32f84928d44bd93a
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-03-08 14:55:13 +0100

    Syscalls are now separate functions

--- high_level.c
+++ high_level.c
@@ -1,11 +1,28 @@
 #include "high_level.h"
 #include "terminal.h"
 #include "user_tasks.h"
+#include "per_cpu.h"
 
+/* This works because hw_context must be the first field of a context. */
+_Static_assert(__builtin_offsetof(struct context,hw_context) == 0);
+
+void __attribute__((regparm(3),noreturn,used)) 
+syscall_yield(struct context *ctx, int syscall_number) {
+  struct context *new_ctx  = sched_choose_from(ctx);
+  hw_context_switch(&new_ctx->hw_context);
+}
+
+void __attribute__((regparm(3),noreturn,used)) 
+syscall_putchar(struct context *ctx, int syscall_number, int arg1) {
+  terminal_putchar(arg1);
+  hw_context_switch(&ctx->hw_context);
+}
+
+  
 void __attribute__((regparm(3),noreturn,used))
 high_level_syscall(struct hw_context *cur_hw_ctx, int syscall_number, int arg1){
-  /* This works because hw_context must be the first field of a context. */
-  _Static_assert(__builtin_offsetof(struct context,hw_context) == 0);
+
+
   struct context *cur_ctx = (struct context *) cur_hw_ctx;
 
   /* terminal_print("ctx %x syscall number %x", cur_ctx, syscall_number); */
@@ -14,16 +31,13 @@ high_level_syscall(struct hw_context *cur_hw_ctx, int syscall_number, int arg1){
   /* terminal_print("Calling interrupt %x\n", cur_hw_ctx); */
   switch(syscall_number){
   case SYSCALL_YIELD:
-    cur_ctx = sched_choose_from(cur_ctx);
-    /* hw_context_switch(&cur_ctx->next->hw_context); */
+    syscall_yield(cur_ctx, syscall_number);
     break;
   case SYSCALL_PUTCHAR:
-    terminal_putchar(arg1);
-    /* terminal_print("After putchar (arg1 %x)\n", arg1); */
-    /* while(1);     */
+    syscall_putchar(cur_ctx, syscall_number, arg1);
     break;
   }
-  hw_context_switch(&cur_ctx->hw_context);
+  fatal("Unknown syscall\n");
 }
 
 void __attribute__((noreturn,used))
@@ -33,7 +47,6 @@ high_level_timer_interrupt_handler(struct hw_context *cur_hw_ctx){
 
   while(1);
 }
-
 
 void context_init(struct context * const ctx, int idx,
                   uint32_t pc,
