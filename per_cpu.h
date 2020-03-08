7649bda85e2b18c33b0adc6250f429bd8fcf868a
Matthieu Lemerre <matthieu.lemerre@cea.fr>
2020-03-08 15:21:10 +0100

    New compilation unit

---
+++ per_cpu.h
@@ -0,0 +1,15 @@
+#ifndef __PER_CPU_H__
+#define __PER_CPU_H__
+
+#include "config.h"
+#include "high_level.h"
+
+struct per_cpu {
+  struct context idle_ctx;
+};
+
+extern struct per_cpu per_cpu[NUM_CPUS];
+
+#define current_cpu() 0
+
+#endif
